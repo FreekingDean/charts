5cc18903f482a7c55c892ba1ae631a31c6187e74
Dean Galvin <deangalvin3@gmail.com>
2024-09-11 18:17:36 -0400

    Add app-template and rennovate

--- updatecharts.sh
+++
@@ -1,4 +0,0 @@
-git submodule update --recursive --remote
-rm -rf charts/*
-cp -r submodules/UnicornTranscoder-chart charts/UnicornTranscoder
-cp -r submodules/kube-plex/charts/kube-plex charts/kube-plex
